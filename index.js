3ccddced0184023cedfee22c76b84175b16fef70
tvoesolncee <tvoesolncee@users.noreply.github.com>
2020-01-23 00:40:20 +0300

    library back (first)

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const body = require('body-parser');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const uuid = require('uuid/v4');
 
 // TODO create es6 class
 
 const app = express();
 app.use(body.json());
+app.use(cookieParser());
 
 // TODO build up cors
 app.use(cors({origin: '*'}));
@@ -30,6 +33,15 @@ const starWarsUser = [
         height: 167,
     }
 ];
+
+const appUsers = new Map();
+const cookieID = new Map();
+
+const books = new Map([
+    [1, {id: 1, title: 'book1', year: 1930, info: 'normas kniga'}],
+    [2, {id: 2, title: 'book2', year: 1885, info: 'ne nravitsya'}],
+    [3, {id: 3, title: 'book3', year: 2000, info: 'wow'}],
+]);
 
 const phrases = [
     {
@@ -78,8 +90,68 @@ app.get('/phrases', function (req, res) {
     res.status(200).json(phrases);
 });
 
+app.get('/books', function (req, res) {
+    res.status(200).json(Array.from(books.values()));
+});
+
+app.delete('/books:id', function (req, res) {
+    const id = req.params;
+    console.log(id);
+    books.delete(id);
+    console.log(books);
+    res.status(200);
+});
 
 // TODO
+
+app.post("/signup", function (req, res) {
+
+    const {mail, passw, gender} = req.body;
+    console.log(mail);
+
+    if (!mail || !passw) return res.status(400).json({error: 'error mail/password'});
+
+    const newUser = {mail, passw, gender};
+    appUsers.set(mail, newUser);
+
+    const id = uuid();
+    cookieID.set(mail, id);
+    res.cookie('sessionid', id);
+
+    return res.status(201).json({mail: mail, gender: gender});
+});
+
+app.post("/login", function (req, res) {
+
+    const {mail, passw} = req.body;
+    const user = {mail, passw};
+
+    if (appUsers.has(mail)) {
+        let registeredUser = appUsers.get(mail);
+        if (registeredUser.mail === mail && registeredUser.passw === passw) {
+            res.cookie("user", user.mail);
+            return res.status(200).json(user);
+        } else {
+            return res.status(400).json({error: 'error mail/password'});
+        }
+    }
+    return res.status(400).json({error: 'no such user'});
+});
+
+app.get("/logout", function (req, res) {
+    res.clearCookie('user');
+    res.send('user logout successfully');
+});
+
+app.get("/me", function (req, res) {
+    const username = req.cookies['user'];
+
+    if (username) {
+        return res.send(username);
+    }
+
+    return res.status(401).json({error: 'no cookie'});
+});
 
 //  /signup (post запоминаем пользователя, выставляем куки -> 201 created)
 //  /login (если у нас есть пользователь, высталяем куки -> 200)

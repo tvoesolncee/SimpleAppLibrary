8c6b3c32178167ea522353b2c3f6735dd3fd121f
tvoesolncee <tvoesolncee@users.noreply.github.com>
2020-02-17 22:25:15 +0300

    one morrre

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ class App {
         this.app = express();
         this.app.use(body.json());
         this.app.use(cors({
-            origin: 'https://tvoesolncee-library-front.herokuapp.com',
-            //origin: 'http://localhost:63342',
+            //origin: 'https://tvoesolncee-library-front.herokuapp.com',
+            origin: 'http://localhost:63342',
             credentials: true
         }));
         this.app.use(cookieParser());
@@ -45,17 +45,21 @@ class App {
         if (!email || !email.match(validEmail) ||
             !password || password.length <= 4 ||
             !gender) {
-            return res.status(400).json({error: 'Некорректные данные пользователя.'});
+            return res.status(400).json({error: 'Не валидные данные пользователя.'});
         }
         if (Array.from(users.keys()).includes(email)) {
             return res.status(400).json({error: 'Пользователь с таким e-mail уже зарегистрирован.'});
         }
-        const user = {email, password, gender};
+
+        const books = new Map();
+
+        const user = {email, password, gender, books};
         users.set(email, user);
         const sessionid = uuid();
         sessions.set(sessionid, email);
         res.cookie('sessionid', sessionid, {expires: new Date(Date.now() + 3600 * 1000)});
         res.status(201).json(user);
+
     };
 
     getUserController = (req, res) => {
@@ -90,49 +94,80 @@ class App {
     };
 
     getBooksController = (req, res) => {
-        if (!req.cookies['sessionid']) {
+        const sessionid = req.cookies['sessionid'];
+        if (!sessionid) {
             return res.status(403).json({error: 'Авторизуйтесь, чтобы просмотреть список книг.'});
         }
-        res.status(200).json({books: Array.from(books.values())});
+
+        const email = sessions.get(sessionid);
+        const user = users.get(email);
+
+        res.status(200).json({books: Array.from(user.books.values())});
     };
 
     addBookController = (req, res) => {
-        if (!req.cookies['sessionid']) {
+        const sessionid = req.cookies['sessionid'];
+        if (!sessionid) {
             return res.status(403).json({error: 'Авторизуйтесь, чтобы добавить новую книгу.'});
         }
-        const {title, year, info} = req.body;
-        if (!title || !year || !info) {
-            return res.status(400).json({error: 'Введите информацию'});
+
+        const email = sessions.get(sessionid);
+        const user = users.get(email);
+
+        const {title, year, info, description} = req.body;
+        if (!title || !year || !info || !description) {
+            return res.status(400).json({error: 'Все поля обязательны'});
         }
+
         const id = uuid();
-        const book = {id, title, year, info};
-        console.log(book);
-        books.set(id, book);
+        const book = {id, title, year, info, description};
+        user.books.set(id, book);
+
+        users.set(email, user);
+
         res.status(201).json(book);
     };
 
     editBookController = (req, res) => {
-        if (!req.cookies['sessionid']) {
+        const sessionid = req.cookies['sessionid'];
+        if (!sessionid) {
             return res.status(403).json({error: 'Авторизуйтесь, чтобы добавить новую книгу.'});
         }
-        const {id, title, year, info} = req.body;
-        if (!title || !year || !info) {
-            return res.status(400).json({error: 'Введите информацию'});
+
+        const {id, title, year, info, description} = req.body;
+        if (!title || !year || !info || !description) {
+            return res.status(400).json({error: 'Все поля обязательны'});
         }
-        const book = {id, title, year, info};
-        books.delete(id);
-        books.set(id, book);
+
+        const email = sessions.get(sessionid);
+        const user = users.get(email);
+        const book = {id, title, year, info, description};
+
+        user.books.delete(id);
+        user.books.set(id, book);
+
+        users.set(email, user);
+
         res.status(201).json(book);
+
     };
 
     deleteBookController = (req, res) => {
-        if (!req.cookies['sessionid']) {
+        const sessionid = req.cookies['sessionid'];
+
+        if (!sessionid) {
             return res.status(403).json({error: 'Авторизуйтесь, чтобы редактировать список книг.'});
         }
         const {id} = req.body;
-        console.log(id);
-        const book = books.get(id);
-        books.delete(id);
+
+        const email = sessions.get(sessionid);
+        const user = users.get(email);
+
+        const book = user.books.get(id);
+        user.books.delete(id);
+
+        users.set(email, user);
+
         res.status(201).json(book);
     };
 }
@@ -144,16 +179,16 @@ const users = new Map([
         email: 'user@user.com',
         password: '1234',
         gender: 'man',
+        books: new Map([
+            [1, {id: 1, title: 'Анна Каренина', year: 1873, info: 'Лев Толстой', description: 'Роман Льва Толстого о трагической любви замужней дамы Анны Карениной и блестящего офицера Вронского на фоне счастливой семейной жизни дворян Константина Лёвина и Кити Щербацкой. Масштабная картина нравов и быта дворянской среды Петербурга и Москвы второй половины XIX века, сочетающая философские размышления авторского alter ego Лёвина с передовыми в русской литературе психологическими зарисовками, а также сценами из жизни крестьян.'}],
+            [2, {id: 2, title: 'Маленький принц', year: 1942, info: 'Антуан де Сент Экзюпери', description: 'Аллегорическая повесть-сказка, наиболее известное произведение Антуана де Сент-Экзюпери. Сказка рассказывает о Маленьком принце, который посещает различные планеты в космосе, включая Землю.'}],
+            [3, {id: 3, title: 'Алые паруса', year: 1923, info: 'Александр Грин', description: 'Повесть-феерия Александра Грина о непоколебимой вере и всепобеждающей, возвышенной мечте, о том, что каждый может сделать для близкого чудо.'}],
+            [4, {id: 4, title: 'Самая красивая женщина в городе', year: 1983, info: 'Чарльз Буковски', description: 'Сборник рассказов американского поэта и писателя контркультуры Чарльза Буковски.'}]
+        ]),
     }]
 ]);
 
 const sessions = new Map([]);
 
-const books = new Map([
-    [1, {id: 1, title: 'Анна Каренина', year: 1873, info: 'Лев Толстой'}],
-    [2, {id: 2, title: 'Маленький принц', year: 1942, info: 'Антуан де Сент Экзюпери'}],
-    [3, {id: 3, title: 'Алые паруса', year: 1923, info: 'Александр Грин'}],
-    [4, {id: 4, title: 'Самая красивая женщина в городе', year: 1983, info: 'Чарльз Буковски'}]
-]);
 
 
